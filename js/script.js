28ae7f554405b507923c17aa6c0cc0adbc9a1959
josel0 <gothangel21@hotmail.com>
2023-03-28 16:04:16 -0500

    commit 1

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// header START
+let menuBtn = document.querySelector("#menu-btn");
+let navBar = document.querySelector(".header .navBar");
+
+menuBtn.addEventListener("click", () => {
+    navBar.classList.toggle("active");
+    menuBtn.classList.toggle("bx-x");
+})
+
+document.addEventListener("click", (event) => {
+    if (event.target.closest(".header")) {
+        return 0;
+    }
+
+    navBar.classList.remove("active");
+    menuBtn.classList.remove("bx-x");
+})
+// header END
+
+// swiperMainPage START
+var swiper = new Swiper(".photoMainPage", {
+    loop: true,
+});
+// swiperMainPage END
